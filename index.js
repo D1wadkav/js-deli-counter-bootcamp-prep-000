08e15e64606c6f25467d13301a63e770946fc51e
Vishal D <vishaldiwadkar@protonmail.com>
2018-09-06 02:09:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
     return "The line is currently empty."
   } else {
     var status = "The line is currently:"
-    for (i = 1; line.length > i; i++) {
+    for (var i = 1; line.length > i; i++) {
       if (i < line.length) {
       status = status + `${i}.` + `${line[i]},`;  
       } else {
