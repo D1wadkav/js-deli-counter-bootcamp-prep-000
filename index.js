d0b83c251b20551fa0d63f603b78b27c310f1505
Vishal D <vishaldiwadkar@protonmail.com>
2018-09-06 01:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  
+  if (line.length === 0) {
+    console.log("There is nobody in lin")
+  }
+  return line.shift()
 }
 
 function currentLine(num) {
